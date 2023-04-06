e58d64cf193114fdb7ab05d3ee69db2e673f3730
Michael Feil <michael.feil@michaelfeil.eu>
2023-04-06 20:49:28 +0200

    reformat

--- pyaudiodetect/iot_utils.py
+++ pyaudiodetect/iot_utils.py
@@ -2,17 +2,18 @@ from PyP100 import PyP100
 import os
 import time
 
+
 class TapoP100Plug:
     def __init__(self, ip: str, email: str, pw: str):
         self.creds = [
             ip if ip else os.environ.get("TAPO_IP", "192.168.1.100"),
             email if email else os.environ.get("TAPO_MAIL", "me@mailbox.org"),
-            pw if pw else os.environ.get("TAPO_PW", "mypw")]
+            pw if pw else os.environ.get("TAPO_PW", "mypw"),
+        ]
         self.connect()
-        
+
     def connect_if_needed(self, delay=True) -> bool:
-        """return if device is on
-        """
+        """return if device is on"""
         try:
             return self.p100.getDeviceInfo()["result"]["device_on"]
         except:
@@ -24,9 +25,7 @@ class TapoP100Plug:
 
     def connect(self) -> bool:
         try:
-            self.p100 = PyP100.P100(
-                *self.creds
-            )
+            self.p100 = PyP100.P100(*self.creds)
             self.p100.handshake()
             self.p100.login()
             return True
@@ -39,9 +38,7 @@ class TapoP100Plug:
             self.p100.turnOn()
 
     def off(self):
-        """return True when reconnect needed
-        """
+        """return True when reconnect needed"""
         device_on = self.connect_if_needed()
         if device_on is True:
             self.p100.turnOff()
-        
--- pyaudiodetect/pyaudiodetect.py
+++ pyaudiodetect/pyaudiodetect.py
@@ -5,37 +5,38 @@ from iot_utils import TapoP100Plug
 import logging
 import argparse
 
- 
+
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
-SHUTDOWN_WAIT_TIME = 180 # seconds
-POOLING_INTERVAL = 5.5 # seconds
+SHUTDOWN_WAIT_TIME = 180  # seconds
+POOLING_INTERVAL = 5.5  # seconds
+
 
 def is_sound_playing() -> bool:
     """
     check if sound is playing by reading the ALSA Sound Card on RaspberryPi
         default for Hifiberry
     """
-    out = subprocess.check_output("cat /proc/asound/card*/pcm*p/sub*/hw_params", shell=True).decode("utf-8")
+    out = subprocess.check_output(
+        "cat /proc/asound/card*/pcm*p/sub*/hw_params", shell=True
+    ).decode("utf-8")
     return not ("closed" in out)
+
 
 # is_sound_playing = lambda t=1: bool(time.time() % 2)
 
+
 def main():
     # settings
-    parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('-i', '--tapo_ip', type=str, help='Description of TAPO_IP')
-    parser.add_argument('-m', '--tapo_mail', type=str, help='Description of TAPO_MAIL')
-    parser.add_argument('-p', '--tapo_pw', type=str, help='Description of TAPO_PW')
+    parser = argparse.ArgumentParser(description="Description of your program")
+    parser.add_argument("-i", "--tapo_ip", type=str, help="Description of TAPO_IP")
+    parser.add_argument("-m", "--tapo_mail", type=str, help="Description of TAPO_MAIL")
+    parser.add_argument("-p", "--tapo_pw", type=str, help="Description of TAPO_PW")
 
     args = parser.parse_args()
     on_countdown = SHUTDOWN_WAIT_TIME
 
-    powerplug = TapoP100Plug(
-        args.tapo_ip,
-        args.tapo_mail,
-        args.tapo_pw
-    )
+    powerplug = TapoP100Plug(args.tapo_ip, args.tapo_mail, args.tapo_pw)
     status = None
 
     while True:
@@ -45,13 +46,13 @@ def main():
 
         if is_sound:
             on_countdown = SHUTDOWN_WAIT_TIME
-            if not status or int(on_countdown/POOLING_INTERVAL) % 3 == 0:
+            if not status or int(on_countdown / POOLING_INTERVAL) % 3 == 0:
                 powerplug.on()
                 logging.info("Now switch plug on")
                 status = True
         else:
             # no sound
-            on_countdown-=POOLING_INTERVAL
+            on_countdown -= POOLING_INTERVAL
 
         if on_countdown < 0:
             powerplug.off()
@@ -60,6 +61,7 @@ def main():
             logging.info("therefore switch plug OFF")
         else:
             logging.info("switch off in % s", on_countdown)
+
 
 if __name__ == "__main__":
     main()
